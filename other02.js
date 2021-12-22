fce5a109853e21c770a2941759854716bbd25e86
mary830331 <sara830331@gmail.com>
2021-12-22 11:03:55 +0800

    修改

---
+++ other02.js
@@ -0,0 +1,215 @@
+export default {
+  type: 'flex',
+  altText: '123456',
+  contents: [
+    {
+      city: '臺北市',
+      district: [
+        { label: '臺北市', id: 'D106', name: '七星山' },
+        { label: '臺北市', id: 'D102', name: '大屯山' },
+        { label: '臺北市', id: 'D107', name: '豬窟山' },
+        { label: '臺北市', id: 'D125', name: '石尖山' },
+        { label: '臺北市', id: 'D108', name: '南港山' },
+        { label: '臺北市', id: 'D103', name: '紗帽山' },
+        { label: '臺北市', id: 'D104', name: '劍潭山' },
+        { label: '臺北市', id: 'D105', name: '蟾蜍山' }
+      ]
+    },
+    {
+      city: '新北市',
+      district: [
+        { label: '新北市', id: 'D115', name: '三角崙山' },
+        { label: '新北市', id: 'D114', name: '三貂嶺' },
+        { label: '新北市', id: 'D112', name: '大桶山' },
+        { label: '新北市', id: 'D109', name: '五分山' },
+        { label: '新北市', id: 'D110', name: '竹子山' },
+        { label: '新北市', id: 'D113', name: '波路山' },
+        { label: '新北市', id: 'D151', name: '基隆山' },
+        { label: '新北市', id: 'D116', name: '燦光寮山' },
+        { label: '新北市', id: 'D111', name: '磺嘴山' }
+      ]
+    },
+    {
+      city: '桃園市',
+      district: [
+        { label: '桃園市', id: 'D136', name: '夫婦山' },
+        { label: '桃園市', id: 'D134', name: '石牛山' },
+        { label: '桃園市', id: 'D137', name: '低陸山' },
+        { label: '桃園市', id: 'D138', name: '良羽鳥山' },
+        { label: '桃園市', id: 'D139', name: '拉拉山' },
+        { label: '桃園市', id: 'D140', name: '東眼山' },
+        { label: '桃園市', id: 'D135', name: '金面山' },
+        { label: '桃園市', id: 'D141', name: '馬望僧侶山' },
+        { label: '桃園市', id: 'D142', name: '插天山' },
+        { label: '桃園市', id: 'D143', name: '塔曼山' },
+        { label: '桃園市', id: 'D124', name: '檜山' }
+      ]
+    },
+    {
+      city: '新竹縣市',
+      district: [
+        { label: '新竹市', id: 'D144', name: '十八尖山' },
+        { label: '新竹縣', id: 'D074', name: '伊澤山' },
+        { label: '新竹縣', id: 'D145', name: '向天湖山' },
+        { label: '新竹縣', id: 'D075', name: '池有山' },
+        { label: '新竹縣', id: 'D149', name: '李棟山' },
+        { label: '新竹縣', id: 'D148', name: '虎禮山' },
+        { label: '新竹縣', id: 'D077', name: '桃山' },
+        { label: '新竹縣', id: 'D150', name: '烏來山' },
+        { label: '新竹縣', id: 'D079', name: '喀拉業山' },
+        { label: '新竹縣', id: 'D146', name: '霞山' },
+        { label: '新竹縣', id: 'D147', name: '霞喀羅大山' }
+      ]
+    },
+    {
+      city: '苗栗縣',
+      district: [
+        { label: '苗栗縣', id: 'D048', name: '大雪山' },
+        { label: '苗栗縣', id: 'D072', name: '大霸尖山' },
+        { label: '苗栗縣', id: 'D049', name: '中雪山' },
+        { label: '苗栗縣', id: 'D050', name: '火石山' },
+        { label: '苗栗縣', id: 'D051', name: '加利山' },
+        { label: '苗栗縣', id: 'D128', name: '加里山' },
+        { label: '苗栗縣', id: 'D129', name: '北坑山' },
+        { label: '苗栗縣', id: 'D130', name: '老松山' },
+        { label: '苗栗縣', id: 'D132', name: '佳仁山' },
+        { label: '苗栗縣', id: 'D131', name: '東陽山' },
+        { label: '苗栗縣', id: 'D126', name: '班山' },
+        { label: '苗栗縣', id: 'D133', name: '能甲山' },
+        { label: '苗栗縣', id: 'D127', name: '馬那邦山' },
+        { label: '苗栗縣', id: 'D078', name: '雪山' }
+      ]
+    },
+    {
+      city: '臺中市',
+      district: [
+        { label: '臺中市', id: 'D084', name: '大劍山' },
+        { label: '臺中市', id: 'D073', name: '小霸尖山' },
+        { label: '臺中市', id: 'D001', name: '中央尖山' },
+        { label: '臺中市', id: 'D002', name: '巴巴山' },
+        { label: '臺中市', id: 'D085', name: '甘薯峰' },
+        { label: '臺中市', id: 'D090', name: '志佳陽大山' },
+        { label: '臺中市', id: 'D091', name: '佳陽山' },
+        { label: '臺中市', id: 'D003', name: '南湖大山' },
+        { label: '臺中市', id: 'D092', name: '南湖大山東峰' },
+        { label: '臺中市', id: 'D004', name: '南湖大山南峰' },
+        { label: '臺中市', id: 'D076', name: '品田山' },
+        { label: '臺中市', id: 'D093', name: '閂山' },
+        { label: '臺中市', id: 'D052', name: '雪山北峰' },
+        { label: '臺中市', id: 'D053', name: '雪山東峰' },
+        { label: '臺中市', id: 'D095', name: '無明山' },
+        { label: '臺中市', id: 'D096', name: '鈴鳴山' },
+        { label: '臺中市', id: 'D097', name: '劍山' },
+        { label: '臺中市', id: 'D054', name: '頭鷹山' }
+      ]
+    },
+    {
+      city: '南投縣',
+      district: [
+        { label: '南投縣', id: 'D020', name: '八通關山' },
+        { label: '南投縣', id: 'D021', name: '大水窟山' },
+        { label: '南投縣', id: 'D022', name: '干卓萬山' },
+        { label: '南投縣', id: 'D023', name: '丹大山' },
+        { label: '南投縣', id: 'D025', name: '六順山' },
+        { label: '南投縣', id: 'D026', name: '玉山北峰' },
+        { label: '南投縣', id: 'D081', name: '玉山西峰' },
+        { label: '南投縣', id: 'D082', name: '玉山東峰' },
+        { label: '南投縣', id: 'D083', name: '玉山前峰' },
+        { label: '南投縣', id: 'D009', name: '白石山' },
+        { label: '南投縣', id: 'D086', name: '白姑大山' },
+        { label: '南投縣', id: 'D011', name: '光頭山' },
+        { label: '南投縣', id: 'D028', name: '合歡山' },
+        { label: '南投縣', id: 'D088', name: '合歡山西峰' },
+        { label: '南投縣', id: 'D029', name: '合歡山東峰' },
+        { label: '南投縣', id: 'D046', name: '西巒大山' },
+        { label: '南投縣', id: 'D030', name: '秀姑巒山' },
+        { label: '南投縣', id: 'D031', name: '卓社大山' },
+        { label: '南投縣', id: 'D014', name: '奇萊主山南峰' },
+        { label: '南投縣', id: 'D032', name: '東郡大山' },
+        { label: '南投縣', id: 'D033', name: '東巒大山' },
+        { label: '南投縣', id: 'D034', name: '牧山' },
+        { label: '南投縣', id: 'D061', name: '南大水窟山' },
+        { label: '南投縣', id: 'D037', name: '能高山主峰' },
+        { label: '南投縣', id: 'D018', name: '能高山南峰' },
+        { label: '南投縣', id: 'D038', name: '能高北峰' },
+        { label: '南投縣', id: 'D039', name: '郡大山' },
+        { label: '南投縣', id: 'D040', name: '馬利加南山' },
+        { label: '南投縣', id: 'D041', name: '馬博拉斯山' },
+        { label: '南投縣', id: 'D094', name: '畢祿山' },
+        { label: '南投縣', id: 'D042', name: '無雙山' },
+        { label: '南投縣', id: 'D043', name: '萬東山西峰' },
+        { label: '南投縣', id: 'D044', name: '義西請馬至山' },
+        { label: '南投縣', id: 'D068', name: '達芬尖山' },
+        { label: '南投縣', id: 'D045', name: '駒盆山' }
+      ]
+    },
+    {
+      city: '嘉義縣',
+      district: [
+        { label: '嘉義縣', id: '10010-D080', name: '玉山' }
+      ]
+    },
+    {
+      city: '高雄市',
+      district: [
+        { label: '高雄市', id: 'D055', name: '三叉山' },
+        { label: '高雄市', id: 'D056', name: '小關山' },
+        { label: '高雄市', id: 'D057', name: '玉山南峰' },
+        { label: '高雄市', id: 'D058', name: '向陽山' },
+        { label: '高雄市', id: 'D059', name: '卑南主山' },
+        { label: '高雄市', id: 'D060', name: '東小南山' },
+        { label: '高雄市', id: 'D035', name: '南玉山' },
+        { label: '高雄市', id: 'D036', name: '南雙頭山' },
+        { label: '高雄市', id: 'D062', name: '庫哈諾辛山' },
+        { label: '高雄市', id: 'D063', name: '海諾南山' },
+        { label: '高雄市', id: 'D064', name: '鹿山' },
+        { label: '高雄市', id: 'D065', name: '雲峰' },
+        { label: '高雄市', id: 'D066', name: '塔芬山' },
+        { label: '高雄市', id: 'D067', name: '塔關山' },
+        { label: '高雄市', id: 'D069', name: '轆轆山' },
+        { label: '高雄市', id: 'D070', name: '關山' },
+        { label: '高雄市', id: 'D071', name: '關山嶺山' }
+      ]
+    },
+    {
+      city: '屏東縣',
+      district: [
+        { label: '屏東縣', id: 'D047', name: '大武山' }
+      ]
+    },
+    {
+      city: '宜蘭縣',
+      district: [
+        { label: '宜蘭縣', id: 'D123', name: '多加神山' },
+        { label: '宜蘭縣', id: 'D117', name: '阿玉山' },
+        { label: '宜蘭縣', id: 'D005', name: '南湖北山' },
+        { label: '宜蘭縣', id: 'D118', name: '盆盆山' },
+        { label: '宜蘭縣', id: 'D119', name: '唐穗山' },
+        { label: '宜蘭縣', id: 'D006', name: '馬比杉山' },
+        { label: '宜蘭縣', id: 'D122', name: '馬望來山' },
+        { label: '宜蘭縣', id: 'D120', name: '望洋山' },
+        { label: '宜蘭縣', id: 'D121', name: '銅山' },
+        { label: '宜蘭縣', id: 'D007', name: '審馬陣山' }
+      ]
+    },
+    {
+      city: '花蓮縣',
+      district: [
+        { label: '花蓮縣', id: 'D024', name: '內嶺爾山' },
+        { label: '花蓮縣', id: 'D008', name: '太魯閣大山' },
+        { label: '花蓮縣', id: 'D098', name: '布拉克桑山' },
+        { label: '花蓮縣', id: 'D027', name: '石門山' },
+        { label: '花蓮縣', id: 'D010', name: '立霧主山' },
+        { label: '花蓮縣', id: 'D087', name: '合歡山北峰' },
+        { label: '花蓮縣', id: 'D012', name: '安東軍山' },
+        { label: '花蓮縣', id: 'D089', name: '羊頭山' },
+        { label: '花蓮縣', id: 'D013', name: '奇萊主山北峰' },
+        { label: '花蓮縣', id: 'D015', name: '奇萊主峰' },
+        { label: '花蓮縣', id: 'D016', name: '帕托魯山' },
+        { label: '花蓮縣', id: 'D017', name: '屏風山' },
+        { label: '花蓮縣', id: 'D099', name: '馬西山' },
+        { label: '花蓮縣', id: 'D100', name: '喀西帕南山' }
+      ]
+    }
+  ]
+}
